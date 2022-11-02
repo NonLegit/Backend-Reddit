7a9b316e77a084f33f6078985511f463f41bed98
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2022-11-02 17:25:07 +0200

    update signup documentation

--- API/controllers/authenticationController.js
+++ API/controllers/authenticationController.js
@@ -3,8 +3,18 @@ class AuthenticationController {
         this.UserServices = UserServices; // can be mocked in unit testing
         this.forgotPassword = this.forgotPassword.bind(this);
         this.forgotUserName = this.forgotUserName.bind(this);
+        this.logout = this.logout.bind(this);
     }
 
+    logout(req, res) {
+        res.cookie("jwt", "loggedout", {
+            expires: new Date(Date.now() + 10 * 1000),
+            httpOnly: true,
+        });
+        res.status(200).json({
+            status: "success",
+        });
+    }
     async forgotPassword(req, res, next) {
         const email = req.body.email;
         const userName = req.body.userName;
@@ -30,9 +40,7 @@ class AuthenticationController {
                 errorMessage: "Provide email",
             });
         } else {
-            const response = await this.UserServices.forgotUserName(
-                email
-            );
+            const response = await this.UserServices.forgotUserName(email);
             res.status(response.status).json(response.body);
         }
     }
