f294717a69263dbe45af423d0bba1b871388b3fa
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2022-11-05 00:43:02 +0200

    setup controller test enviroment

--- API/test/controllers/authenticationController.js
+++ API/test/controllers/authenticationController.js
@@ -1,47 +1,46 @@
 const assert = require("chai").assert;
-const { expect } = require("chai");
-const { mockRequest, mockResponse } = require("mock-req-res");
-const { stub, match } = require("sinon");
-const proxyquire = require("proxyquire");
+const expect = require("chai").expect;
+const request = require("supertest");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config/config.env" });
+process.env.NODE_ENV = "test";
 
-const AuthenticationController = require("./../../controllers/AuthenticationController");
-const userServiceObj = {
-    signUp: function (email, userName, password) {
-        const response = {
-            status: 400,
-            body: {
-                errorMessage: "User already Exists",
-            },
-        };
-        return response;
-    },
-};
-const authenticationControllerObj = new AuthenticationController(
-    userServiceObj
-);
+const app = require("./../../app");
+// const { mockRequest, mockResponse } = require("mock-req-res");
+// const { stub, match } = require("sinon");
+// const proxyquire = require("proxyquire");
 
 describe("Authentication Controller Test", () => {
     describe("Sign-up Test", () => {
-        // const mockSave = stub();
-
-        // const res = mockResponse();
-        // const resetStubs = () => {
-        //     mockSave.resetHistory();
-        //     res.json.resetHistory();
-        // };
-        // const expected = { name: "", description: "", id: 1 };
-        // before(() => {
-        //     //save.returns(expected);
-        // });
         it("first test", async () => {
-            // const req = mockRequest({
-            //     body: { email: "", password: "", userName: "" },
-            // });
-
-            // await authenticationControllerObj.signUp(req, res);
-            // console.log(res);
+            request(app)
+                .post("/api/v1/users/logout")
+                .send()
+                .then((res) => {
+                    console.log(res.cookie);
+                });
             assert.equal(true, true);
             // expect(res.json).to.have.been.calledWith(match(expected));
         });
     });
 });
+
+// const mockSave = stub();
+
+// const res = mockResponse();
+// const resetStubs = () => {
+//     mockSave.resetHistory();
+//     res.json.resetHistory();
+// };
+// const expected = { name: "", description: "", id: 1 };
+// before(() => {
+//     //save.returns(expected);
+// });
+//it
+
+// const req = mockRequest({
+//     body: { email: "", password: "", userName: "" },
+// });
+
+// await authenticationControllerObj.signUp(req, res);
+// console.log(res);
