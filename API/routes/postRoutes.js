a8610b9ee2c47bfcea745d73fcff420eb0fefda8
doaa281 <65799105+doaa281@users.noreply.github.com>
2022-12-14 00:33:15 +0200

    server key

--- API/routes/postRoutes.js
+++ API/routes/postRoutes.js
@@ -20,11 +20,11 @@ router
 router.route("/:postId/follow_post").patch(PostController.followPost);
 router.route("/:postId/spam").patch(PostController.spam);
 
-//router.param("postId", PostController.mustBeAuthOrMod);
+router.param("postId", PostController.mustBeAuthOrMod);
 
 router.route("/:postId/actions/:action").patch(PostController.postActions);
 
-//router.param("postId", PostController.mustBeMod);
+router.param("postId", PostController.mustBeMod);
 
 router.route("/:postId/moderate/:action").patch(PostController.moderatePost);
 router.route("/:postId/:suggested_sort").patch(PostController.suggestedSort);
