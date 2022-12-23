5af8571e674f95de1ec704a9c5d88fb0e62a3d9d
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2022-12-23 20:16:41 +0200

    add comment controller unit tests

--- API/test/controllers/commentController.js
+++ API/test/controllers/commentController.js
@@ -247,4 +247,785 @@ describe("Comment Controller CRUD operations", () => {
       });
     });
   });
+
+  describe("commentVote Test", () => {
+    it("first test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        addVote: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.commentVote(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(200).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("second test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        addVote: (me, posts) => {
+          return false;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.commentVote(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(304).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("thrid test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return true;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        addVote: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.commentVote(req, res, "");
+      expect(res.status).to.have.been.calledWith(405);
+      expect(res.status(405).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Method Not Allowed",
+      });
+    });
+    it("fourth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: false,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        addVote: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.commentVote(req, res, "");
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.status(404).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Comment Not Found",
+      });
+    });
+    it("fifth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 2,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        addVote: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.commentVote(req, res, "");
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.status(400).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Enter Valid Vote dir",
+      });
+    });
+  });
+
+  describe("saveComment Test", () => {
+    it("first test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        saveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.saveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(200).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("second test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        saveComment: (me, posts) => {
+          return false;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.saveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(304).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("thrid test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return true;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        saveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.saveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(405);
+      expect(res.status(405).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Method Not Allowed",
+      });
+    });
+    it("fourth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: false,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        saveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.saveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.status(404).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Comment Not Found",
+      });
+    });
+    it("fifth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 2,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        saveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.saveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.status(400).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Enter Valid Vote dir",
+      });
+    });
+  });
+
+  describe("unSaveComment Test", () => {
+    it("first test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        unSaveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.unSaveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(200).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("second test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        unSaveComment: (me, posts) => {
+          return false;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.unSaveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(304).json).to.have.been.calledWith({
+        status: "success",
+      });
+    });
+    it("thrid test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return true;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        unSaveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.unSaveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(405);
+      expect(res.status(405).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Method Not Allowed",
+      });
+    });
+    it("fourth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 1,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: false,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        unSaveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.unSaveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.status(404).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Comment Not Found",
+      });
+    });
+    it("fifth test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          postId: "Ahmed",
+        },
+        body: {
+          dir: 2,
+        },
+      };
+      const UserService = {
+        checkBlockStatus: async (user, me) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        findCommentById: async (postId) => {
+          let response = {
+            success: true,
+            data: {
+              author: "1",
+              votes: "5",
+            },
+          };
+          return response;
+        },
+        unSaveComment: (me, posts) => {
+          return true;
+        },
+      };
+
+      const commentController = new CommentController({
+        CommentService,
+        UserService,
+      });
+      await commentController.unSaveComment(req, res, "");
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.status(400).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "Enter Valid Vote dir",
+      });
+    });
+  });
+
+  describe("getUserComments Test", () => {
+    it("first test success", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          userName: "Ahmed",
+        },
+        query: {
+          sort: "New",
+          limit: -1,
+          page: -1,
+        },
+      };
+      const UserService = {
+        getUserByName: async (userName, dummy) => {
+          return {
+            success: true,
+            data: {
+              _id: "2",
+            },
+          };
+        },
+        checkBlockStatus: async (me, user) => {
+          return false;
+        },
+      };
+      const CommentService = {
+        getUserComments: async (userid, sorttype) => {
+          let posts = [
+            {
+              _id: "1",
+            },
+          ];
+          return posts;
+        },
+      };
+
+      const commentController = new CommentController({ CommentService, UserService });
+      await commentController.getUserComments(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(200).json).to.have.been.calledWith({
+        status: "success",
+        posts: [
+          {
+            _id: "1",
+          },
+        ],
+      });
+    });
+    it("third test fail", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          userName: "ahmed",
+        },
+        query: {
+          sortType: "New",
+        },
+      };
+      const UserService = {
+        getUserByName: async (userName, dummy) => {
+          return {
+            success: false,
+          };
+        },
+        checkBlockStatus: async (me, user) => {
+          return false;
+        },
+      };
+      const CommentService = {};
+      const commentController = new CommentController({ CommentService, UserService });
+      await commentController.getUserComments(req, res, "");
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.status(404).json).to.have.been.calledWith({
+        status: "fail",
+        errorMessage: "User Not Found",
+      });
+    });
+    it("fourth test fail", async () => {
+      const req = {
+        user: {
+          _id: "1",
+        },
+        params: {
+          userName: "ahmed",
+        },
+        query: {
+          sortType: "New",
+        },
+      };
+      const UserService = {
+        getUserByName: async (userName, dummy) => {
+          return {
+            success: true,
+            data: {
+              _id: "2",
+            },
+          };
+        },
+        checkBlockStatus: async (me, user) => {
+          return true;
+        },
+      };
+      const CommentService = {};
+      const commentController = new CommentController({ CommentService, UserService });
+      await commentController.getUserComments(req, res, "");
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.status(200).json).to.have.been.calledWith({
+        status: "success",
+        posts: [],
+      });
+    });
+  });
 });
